6287bd2a5ee698c3478827732a0cc15f04c52467
TheDrOfDoctoring <bennetanstee@gmail.com>
2023-09-02 12:16:13 +0400

    silly BloodStats redirect

--- src/main/java/com/doctor/vampiricageing/mixin/BloodStatsMixin.java
+++ src/main/java/com/doctor/vampiricageing/mixin/BloodStatsMixin.java
@@ -2,8 +2,11 @@ package com.doctor.vampiricageing.mixin;
 
 import com.doctor.vampiricageing.capabilities.VampiricAgeingCapabilityManager;
 import com.doctor.vampiricageing.config.CommonConfig;
+import de.teamlapen.vampirism.api.entity.player.vampire.IBloodStats;
 import de.teamlapen.vampirism.core.ModAttributes;
 import de.teamlapen.vampirism.entity.player.vampire.BloodStats;
+import net.minecraft.world.entity.ai.attributes.Attribute;
+import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,19 +17,26 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BloodStats.class)
-public class BloodStatsMixin {
-
-    @Shadow @Final private Player player;
+public abstract class BloodStatsMixin implements IBloodStats {
 
     //Forcing vampirism to take into account the Age based multiplier for blood exhaustion.
 
-    @Inject(method = "addExhaustion(FZ)V", at = @At(value = "HEAD"), remap = false)
-    void addExhaustion(float amount, boolean ignoreModifier, CallbackInfo ci) {
-        if(CommonConfig.shouldAgeAffectExhaustion.get() && ignoreModifier) {
-            if(player.getAttribute(ModAttributes.BLOOD_EXHAUSTION.get()).getModifier(VampiricAgeingCapabilityManager.EXHAUSTION_UUID) != null) {
+    @Shadow @Final private Player player;
+
+    @Shadow private float bloodExhaustionLevel;
+
+    @Redirect(method = "onUpdate", at = @At(value = "INVOKE", target = "Lde/teamlapen/vampirism/entity/player/vampire/BloodStats;addExhaustion(FZ)V"), remap = false)
+    private void addExhaustion(BloodStats bloodStats, float amount, boolean ignoreModifier) {
+        if (!ignoreModifier) {
+            AttributeInstance attribute = player.getAttribute(ModAttributes.BLOOD_EXHAUSTION.get());
+            amount *= attribute.getValue();
+        }
+        else if(CommonConfig.shouldAgeAffectExhaustion.get()) {
+            if(this.player.getAttribute(ModAttributes.BLOOD_EXHAUSTION.get()).getModifier(VampiricAgeingCapabilityManager.EXHAUSTION_UUID) != null) {
                 double exhaustionMult = player.getAttribute(ModAttributes.BLOOD_EXHAUSTION.get()).getModifier(VampiricAgeingCapabilityManager.EXHAUSTION_UUID).getAmount();
                 amount = (float) (amount * exhaustionMult);
             }
         }
+        this.bloodExhaustionLevel = Math.min(this.bloodExhaustionLevel + amount, 40.0F);
     }
 }
